de6bf6719864a973ca71aef39f03a4164858c414
Pierre Denauw <pierre.denauw@telecomnancy.eu>
2017-05-10 15:48:39 +0200

    Ébauche de l'IA / Première version du Negamax avec élagage AlphaBeta / Seul le coeur de l'algorithme est réalisé, il manque la plupart des fonctions essentielles d'évaluation de valeurs d'un plateau, et de génération des futures mouvements possibles

---
+++ ia.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int value(B){
+}
+
+int negamaxAB(Board B,Coord lastMove, int A, int B, int depth){
+	if ((resolveGame(B, lastMove) == Bot)||(resolveGame(P, lastMove) == WhitePlayer)||(resolveGame(P, lastMove) == Draw)||(depth == 0)){
+		return moveValue(B);
+	}
+	else
+	{
+		bestValue = NULL;
+		int count = 0;
+		State* nextBoards = nextState(B, &count); //penser à mettre dans count le nombre d'éléments
+		for (int i = 0; i < count; ++i)
+		{
+			nextBoard = stateToBoard(nextBoards[i]);
+			value = -negamaxAB(nextBoard, lastMove, -B, -A);
+			if (bestValue == NULL){
+				bestValue == value;
+			} else if (value > bestValue) {
+				bestValue = value;
+				if (bestValue>A){
+					A = bestValue;
+					if (A>B){
+						return bestValue;
+					}
+				}
+			}
+		}
+		return bestValue
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
