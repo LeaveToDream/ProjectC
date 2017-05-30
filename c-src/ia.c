5e5bdc510b30fac38bbd35350f83d934ce65d0e2
Charlotte Asselin Boullé <charlotte.boulle@telecomnancy.net>
2017-05-31 01:21:12 +0200

    bon ça ne recompile toujours pas mais déjà y'a plus d'erreur (enfin elles sont cachées ;p), si besoin je vais mettre une version fonctionnelle (mais sans IA) sur ma branche ;)

--- c-src/ia.c
+++ c-src/ia.c
@@ -8,66 +8,50 @@
 
 ValuedMove negamaxAB_IA(Board b, int A, int B, int depth, int difficulty, Pawn color, Move lastMove){
     int moveValuation, alpha, beta;
-    int* count;
+//Inutile    int* count;
     ValuedMove bestMove, newMove;
     bestMove.value = -100;
     bestMove.move = initMove(initCoord(-1,-1),initCoord(-1,-1));
     alpha = A;
     beta = B;
-	switch(resolveGame(&b)){
-		case Bot :
-			return initValuedMove(moveValue(b, Bot, depth),lastMove);
-		case WhitePlayer :
-			return initValuedMove(moveValue(b, WhitePlayer, depth),lastMove);
-		case Draw :
-			return initValuedMove(moveValue(b, Draw, depth),lastMove);
-		case Playing :
-			if (depth == difficulty){
-				return initValuedMove(moveValue(b, Playing, depth),lastMove);
-			}
-			int bestValue = -100;
-			int count = 0;
+	  switch(resolveGame(&b)){
+      case Bot :
+			   return initValuedMove(moveValue(b, Bot, depth),lastMove);
+		  case WhitePlayer :
+			   return initValuedMove(moveValue(b, WhitePlayer, depth),lastMove);
+		  case Draw :
+			   return initValuedMove(moveValue(b, Draw, depth),lastMove);
+		  case Playing :
+			   if (depth == difficulty){
+			        return initValuedMove(moveValue(b, Playing, depth),lastMove);
+			   }
+			   int bestValue = -100;
+			   int count = 0;
 
-            Move listPossibleMove[144];
-            initListPossibleMove(&listPossibleMove);
-            generateListePossibleMove(&listPossibleMove, b, &count, color);
-			for (int i = 0; i < count; ++i)
-			{
-				Board nextBoard = copyBoard(b);
-                movePawn(nextBoard, listPossibleMove[i].from, listPossibleMove[i].to);
-				moveValuation = -negamaxAB(nextBoard, -beta, -alpha, depth + 1, difficulty, enemyPawn(couleur));
-                if (moveValuation > bestValue){
-                    bestValue = moveValuation;
-                    if (bestValue > alpha){
-                        alpha = bestValue;
-                        if (alpha>beta){
-                            return bestValue;
-			*count = 0;
-
-            Move listPossibleMove[144];
-            initListPossibleMove(listPossibleMove);
-            generateListePossibleMove(listPossibleMove, b, count, color);
-			for (int i = 0; i < *count; ++i)
-			{
-				Board nextBoard = copyBoard(b);
-                movePawn(nextBoard, listPossibleMove[i].from, listPossibleMove[i].to);
-				newMove = negamaxAB_IA(nextBoard, -beta, -alpha, depth + 1, difficulty, enemyPawn(color), listPossibleMove[i]);
-                newMove.value = -newMove.value;
-                if (moveValuation > bestMove.value){
-                    bestMove.value = moveValuation;
-                    bestMove.move = listPossibleMove[i];
-                    if (bestMove.value > alpha){
-                        alpha = bestMove.value;
-                        if (alpha>beta){
-                            return bestMove;
-                        }
-                    }
-                }
-			}
-			return bestMove;
-	}
+         Move listPossibleMove[144];
+         initListPossibleMove(listPossibleMove);
+         generateListePossibleMove(listPossibleMove, b, count, color);
+			   for (int i = 0; i < count; ++i){
+           Board nextBoard = copyBoard(b);
+           movePawn(nextBoard, listPossibleMove[i].from, listPossibleMove[i].to);
+				   newMove = negamaxAB_IA(nextBoard, -beta, -alpha, depth + 1, difficulty, enemyPawn(color), listPossibleMove[i]);
+           newMove.value = -newMove.value;
+           if (moveValuation > bestMove.value){
+             bestMove.value = moveValuation;
+             bestMove.move = listPossibleMove[i];
+             if (bestMove.value > alpha){
+               alpha = bestMove.value;
+               if (alpha>beta){
+                 return bestMove;
+               }
+             }
+           }
+	       }
+			   return bestMove;
+	  }
 }
-
+//Inutile
+/*
 int moveValue(Board B, Status s, int depth){
     switch(s){
         case Bot :
@@ -81,16 +65,18 @@ int moveValue(Board B, Status s, int depth){
         default:
             exit(0);
     }
-}
-
+}*/
+//Inutile
+/*
 void initListPossibleMove(Move* listPossibleMove){
     Coord empty = initCoord(-1,-1);
     for (int i = 0; i < 144; i++) {
         listPossibleMove[i] = initMove(empty,empty);
     }
-}
+}*/
 
-
+//Inutile
+/*
 int generateListePossibleMove(Move* listPossibleMove, Board b, int* count, Pawn color){
     Coord listPawn[9];
     int pawnCount = 0;
@@ -124,15 +110,16 @@ int generateListePossibleMove(Move* listPossibleMove, Board b, int* count, Pawn 
         }
     }
     return 0;
-}
+}*/
 
 //Inutile
-/*Move initMove(Coord c1, Coord c2){
+Move initMove(Coord c1, Coord c2){
     Move m;
     m.from = c1;
     m.to = c2;
     return m;
-}*/
+}
+//Inutile
 
 ValuedMove initValuedMove(int value, Move move){
     ValuedMove res;
@@ -140,6 +127,7 @@ ValuedMove initValuedMove(int value, Move move){
     res.move = move;
     return res;
 }
+//Inutile
 
 void initListPossibleMove(Move* listPossibleMove){
     Coord empty = initCoord(-1,-1);
@@ -183,11 +171,13 @@ int generateListePossibleMove(Move* listPossibleMove, Board b, int* count, Pawn 
         }
     }
     return 0;
-}
+}*/
 
+//Inutile
+/*
 Move initMove(Coord c1, Coord c2){
     Move m;
     m.from = c1;
     m.to = c2;
     return m;
-}
+}*/
