9d6270dbb5821a439a8f9c436fdc040fc31d42bc
Pierre Denauw <pierre.denauw@telecomnancy.eu>
2017-05-31 16:21:20 +0200

    Cleaning debugging commentary in IA code;

--- c-src/ia.c
+++ c-src/ia.c
@@ -8,7 +8,6 @@
 #include "../c-head/board.h"
 
 ValuedMove negamaxAB_IA(Board b, int A, int B, int depth, int difficulty, Pawn color, Move lastMove){
-    printf("Appel negamax avec profondeur %d\n", depth);
     int alpha, beta, countBestMove = 1;
     int count = 0;
     ValuedMove bestMove, newMove;
@@ -36,47 +35,16 @@ ValuedMove negamaxAB_IA(Board b, int A, int B, int depth, int difficulty, Pawn c
 
             Move listPossibleMove[144];
             initListPossibleMove(listPossibleMove);
-            //printf("Initilisation en (-1,-1)\n");
-            for (int j = 0; j < 144; j++) {
-                if (!(compareCoord(listPossibleMove[j].from, initCoord(-1,-1)))){
-                    //printf("La pièce en (%d,%d) peut aller en (%d,%d)\n", listPossibleMove[j].from.x,listPossibleMove[j].from.y,listPossibleMove[j].to.x,listPossibleMove[j].to.y);
-                } else {
-                    //printf("+");
-                }
-            }
-            //printf("\n");
+
             count = generateListePossibleMove(listPossibleMove, b, color);
-            //printf("Après génération\n");
-            for (int j = 0; j < 144; j++) {
-                if (!(compareCoord(listPossibleMove[j].from, initCoord(-1, -1)))) {
-                    //printf("La pièce en (%d,%d) peut aller en (%d,%d)\n", listPossibleMove[j].from.x, listPossibleMove[j].from.y, listPossibleMove[j].to.x, listPossibleMove[j].to.y);
-                } else {
-                    //printf("+");
-                }
-            }
-            //printf("\n");
-            //printf("Longueur move possible : %d\n", count);
 			for (int i = 0; i < count; ++i)
 			{
-                //printf("Affichage du board :");
-                //printf("\n");
-                //display(b);
 				Board nextBoard = copyBoard(b);
-                //printf("Affichage de la copie :");
-                //printf("\n");
-                //display(nextBoard);
                 movePawn(nextBoard, listPossibleMove[i].from, listPossibleMove[i].to);
-                //printf("Affichage de la copie après déplacement :");
-                //printf("\n");
-                //display(nextBoard);
-                //printf("La pièce en (%d,%d) va en (%d,%d)\n", listPossibleMove[i].from.x, listPossibleMove[i].from.y, listPossibleMove[i].to.x, listPossibleMove[i].to.y);
                 resolveMove(&nextBoard, listPossibleMove[i].to);
                 refreshPMAfterMove(b, listPossibleMove[i].to);
 				newMove = negamaxAB_IA(nextBoard, -beta, -alpha, depth + 1, difficulty, enemyPawn(color), listPossibleMove[i]);
                 freeBoard(nextBoard);
-                //newMove.value = -newMove.value;
-                //printf("Valeur du move : %d\n", newMove.value);
-                //printf("Valeur du meilleur move : %d\n", bestMove.value);
                 if (newMove.value > bestMove.value){
                     countBestMove = 1;
                     bestMove.value = newMove.value;
@@ -125,13 +93,11 @@ int generateListePossibleMove(Move* listPossibleMove, Board b, Pawn color){
     int pawnCount = 0;
     int count = 0;
     int n;
-    //display(b);
     for (int i = 0; i < b.length; i++) {
         for (int j = 0; j < b.length; j++) {
             if (b.board[i][j] == color){
                 listPawn[pawnCount] = initCoord(i,j);
                 pawnCount++;
-                //printf("Pawn found in (%d,%d)\n",i,j);
             }
         }
     }
@@ -150,7 +116,6 @@ int generateListePossibleMove(Move* listPossibleMove, Board b, Pawn color){
         while (!(compareCoord(b.possibleMove[listPawn[k].x][listPawn[k].y][n], initCoord(-1,-1)))&&count<144){
             listPossibleMove[count] = initMove(listPawn[k],b.possibleMove[listPawn[k].x][listPawn[k].y][n]);
             count++;
-            //printf("Count : %d\n", count);
             n++;
         }
     }
